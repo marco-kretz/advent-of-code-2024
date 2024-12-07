a55d042041a029922aea873ccd12619f53ca65b2
Marco Kretz <marco@codebites.de>
2024-12-07 23:01:54 +0100

    Sped up equation test by not building each equation

--- src/AbstractTask.php
+++ src/AbstractTask.php
@@ -9,7 +9,6 @@ abstract class AbstractTask
     }
 
     abstract public function parseInput(): array;
-    abstract public function solve(): string;
     abstract public function solvePartOne(array $input): string;
     abstract public function solvePartTwo(array $input): string;
 }
