c05f1ff19eb877e81ad7cb1cb17e3ba9088127d0
Marco Kretz <marco@codebites.de>
2024-12-08 14:26:02 +0100

    Day8 - Part Two

--- src/Day8.php
+++ src/Day8.php
@@ -33,22 +33,15 @@ class Day8 extends AbstractTask
                     $dy = $pair[1][1] - $pair[0][1];
 
                     // Calculate first antinode
-                    $antinode1 = [($pair[0][0] - $dx), ($pair[0][1] - $dy)];
-                    // Check if it's within the map
-                    if (
-                        ($antinode1[0] >= 0 && $antinode1[0] < $cols) &&
-                        ($antinode1[1] >= 0 && $antinode1[1] < $rows)
-                    ) {
+                    $antinode1 = $this->getNextAntinode($pair[0], [$cols, $rows], -$dx, -$dy);
+                    if ($antinode1 !== null) {
                         $antinodes["{$antinode1[0]}/{$antinode1[1]}"] = true;
                     }
 
                     // Calculate second antinode
-                    $antinode2 = [($pair[1][0] + $dx), ($pair[1][1] + $dy)];
+                    $antinode2 = $this->getNextAntinode($pair[1], [$cols, $rows], $dx, $dy);
                     // Check if it's within the map
-                    if (
-                        ($antinode2[0] >= 0 && $antinode2[0] < $cols) &&
-                        ($antinode2[1] >= 0 && $antinode2[1] < $rows)
-                    ) {
+                    if ($antinode2 !== null) {
                         $antinodes["{$antinode2[0]}/{$antinode2[1]}"] = true;
                     }
                 }
@@ -63,7 +56,46 @@ class Day8 extends AbstractTask
      */
     public function solvePartTwo(array $parsedInput): string
     {
-        return '';
+        ['rows' => $rows, 'cols' => $cols, 'antennas' => $antennas] = $parsedInput;
+        $antinodes = [];
+
+        foreach ($antennas as $type => $locations) {
+            // Match all unique antenna pairs
+            for ($i = 0; $i < count($locations); $i++) {
+                for ($j = count($locations) - 1; $j >= 0; $j--) {
+                    if ($i >= $j) {
+                        continue;
+                    }
+
+                    // Current antenna pair
+                    $pair = [$locations[$i], $locations[$j]];
+
+                    // Add current antenna to antinodes list
+                    $antinodes["{$pair[0][0]}/{$pair[0][1]}"] = true;
+                    $antinodes["{$pair[1][0]}/{$pair[1][1]}"] = true;
+
+                    // Calculate the delta (distance) of the pair
+                    $dx = $pair[1][0] - $pair[0][0];
+                    $dy = $pair[1][1] - $pair[0][1];
+
+                    // Negative dx/dy direction
+                    $current = $pair[0];
+                    while (($nextAntinode = $this->getNextAntinode($current, [$cols, $rows], -$dx, -$dy)) !== null) {
+                        $antinodes["{$nextAntinode[0]}/{$nextAntinode[1]}"] = true;
+                        $current = $nextAntinode;
+                    }
+
+                    // Positive dx/dy direction
+                    $current = $pair[1];
+                    while (($nextAntinode = $this->getNextAntinode($current, [$cols, $rows], $dx, $dy)) !== null) {
+                        $antinodes["{$nextAntinode[0]}/{$nextAntinode[1]}"] = true;
+                        $current = $nextAntinode;
+                    }
+                }
+            }
+        }
+
+        return (string) count($antinodes);
     }
 
     public function parseInput(): array
@@ -74,7 +106,7 @@ class Day8 extends AbstractTask
         foreach (file($this->input, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $line) {
             $row = str_split($line);
             foreach ($row as $index => $element) {
-                // We only need to save antenna locations
+                // We only need to save antenna locations, ignore the rest
                 if ($element === '.') {
                     continue;
                 }
@@ -97,5 +129,28 @@ class Day8 extends AbstractTask
             'cols' => $colCount,
             'antennas' => $antennaLocations,
         ];
+    }
+
+    /**
+     * Calculates the next possible antinode within the map's boundaries.
+     *
+     * @param array $current Current antennas position
+     * @param array $bounds Map boundaries
+     * @param int $dx
+     * @param int $dy
+     *
+     * @return array New antinode's position, null if not possible
+     */
+    private function getNextAntinode(array $current, array $bounds, int $dx, int $dy): ?array
+    {
+        $antinode = [$current[0] + $dx, $current[1] + $dy];
+        if (
+            ($antinode[0] >= 0 && $antinode[0] < $bounds[0]) &&
+            ($antinode[1] >= 0 && $antinode[1] < $bounds[1])
+        ) {
+            return $antinode;
+        }
+
+        return null;
     }
 }
